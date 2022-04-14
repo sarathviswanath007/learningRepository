eaffb1d228af02d3a8a2a51af03d33c3257156ac
sarath <sarathviswanath93@gmail.com>
2022-04-14 14:49:52 +0530

    done

--- objects.js
+++ objects.js
@@ -9,3 +9,4 @@ this.isHealthy=isHealthy;
 //create an object for this class
 const favoriteFood = new FavoriteFood("friedrice",false);
 //define the properties of favroite food. How to do it? Using constructors
+//hello //hi
